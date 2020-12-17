5e996e93e06c94e79ba2df903b3e2e101abd5534
Ssioo <wooisso@naver.com>
2020-12-17 20:10:39 +0900

    create mock user/history

--- Covid/src/routers/user.ts
+++ Covid/src/routers/user.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { sendErr, sendRes } from '../utils/response-handler'
 import { userDao } from '../daos/UserDao'
+import moment from "moment";
 
 const router = Router()
 
@@ -18,6 +19,39 @@ router.post('/sign', async (req, res) => {
         sendRes(res, 200, '', {
             success: true
         })
+    } catch (e) {
+        console.log(e)
+        sendErr(res, e)
+    }
+})
+
+router.get('/history', async (req, res) => {
+    try {
+        const {uuid} = req.query
+        // TODO : Replace Mocking
+        sendRes(res, 200, '성공', [
+                {
+                    id: 1,
+                    storeId: 1,
+                    address: '서울시 동작구 상도로',
+                    latLng: {
+                        latitude: 127.342311,
+                        longitude: 37.3222102,
+                    },
+                    createdAt: moment().toJSON()
+                },
+                {
+                    id: 2,
+                    storeId: 1,
+                    address: '서울시 동작구 상도로',
+                    latLng: {
+                        latitude: 127.342311,
+                        longitude: 37.3222102,
+                    },
+                    createdAt: moment().toJSON()
+                }
+            ]
+        )
     } catch (e) {
         console.log(e)
         sendErr(res, e)
