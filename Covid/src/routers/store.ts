113d490c878453f2b09b87dffc9dd8e22d45b68a
Ssioo <wooisso@naver.com>
2020-12-17 20:48:14 +0900

    fix missing import statements on router

--- Covid/src/routers/store.ts
+++ Covid/src/routers/store.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { sendErr, sendRes } from '../utils/response-handler'
-import { userDao } from '../daos/StoreDao'
+import { storeDao } from '../daos/StoreDao'
 
 const router = Router()
 
@@ -8,7 +8,7 @@ router.post('/register', async (req, res) => {
     try {
         const { name, phone, address } = req.body
         // TODO: 이미 존재하는 가게인지 중복 검사.
-        const result = await storeDao.insertUser(name, phone, address)
+        const result = await storeDao.insertStore(name, phone, address)
         if (!result) throw new Error()
         sendRes(res, 200, '', {
             success: true
