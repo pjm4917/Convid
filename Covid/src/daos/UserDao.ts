2ba29953e84d9b28d0bbe8f6590358733105cce5
Yeonwoo_Cho <41153640+Ssioo@users.noreply.github.com>
2020-12-17 20:43:29 +0900

    create user/sign api (#2)
    * create user/sign
    
    * update success message
    
    * only update new token or new os is arrived
    
    * fix duplicate update error

--- Covid/src/daos/UserDao.ts
+++ Covid/src/daos/UserDao.ts
@@ -1,9 +1,19 @@
 import { BaseDao } from './BaseDao'
 
 class UserDao extends BaseDao {
-    insertUser(token: string, uuid: string, os: string | null) {
+    insertUser(token: string, uuid: string, os: string) {
         const queryStr = 'INSERT INTO USER_TB (token, uuid, os) VALUES (?, ?, ?)'
         return this.insert(queryStr, [token, uuid, os])
+    }
+
+    getUserByUUID(uuid: string) {
+        const queryStr = 'SELECT * FROM USER_TB WHERE uuid = ? AND deleted_at IS NULL'
+        return this.getOne(queryStr, [uuid])
+    }
+
+    deleteUserByUUID(uuid: string) {
+        const queryStr = 'UPDATE USER_TB SET deleted_at = NOW() WHERE uuid = ? AND deleted_at IS NULL'
+        return this.update(queryStr, [uuid])
     }
 }
 
--- Covid/src/routers/user.ts
+++ Covid/src/routers/user.ts
@@ -12,10 +12,22 @@ router.get('/test', async (req, res) => {
 router.post('/sign', async (req, res) => {
     try {
         const { token, uuid, os } = req.body
-        // TODO: 이미 존재하는 유저인지 중복 검사.
+        if (!token || !uuid || !os) throw new Error('No Appropriate fields in Request')
+        const found = await userDao.getUserByUUID(uuid)
+        if (found) {
+            console.log(found)
+            if (found.token !== token && found.os !== os) {
+                await userDao.deleteUserByUUID(uuid)
+                await userDao.insertUser(token, uuid, os)
+            }
+            sendRes(res, 201, '이미 존재하는 유저입니다', {
+                updated: true
+            })
+            return
+        }
         const result = await userDao.insertUser(token, uuid, os)
         if (!result) throw new Error()
-        sendRes(res, 200, '', {
+        sendRes(res, 200, '성공', {
             success: true
         })
     } catch (e) {
