6e9211ffea695cc6b851934bf755aaea5e6147f2
pjm4917 <pjm4917@gmail.com>
2020-12-17 23:30:49 +0900

    store 부분 수정

--- Covid/src/daos/StoreDao.ts
+++ Covid/src/daos/StoreDao.ts
@@ -1,10 +1,21 @@
 import { BaseDao } from './BaseDao'
 
 class StoreDao extends BaseDao {
-    insertStore(name: string, phone: string, address: string | null) {
-        const queryStr = 'INSERT INTO STORE_TB (name, phone, address) VALUES (?, ?, ?)'
-        return this.insert(queryStr, [name, phone, address])
+    insertStore(name: string, pwd: stirng, phone: string, address: string, latitude: string, longitude: string) {
+        const queryStr = 'INSERT INTO STORE_TB (name, pwd, phone, address, latitude, longitude) VALUES (?, ?, ?, ?, ?, ?)'
+        return this.insert(queryStr, [name, pwd, phone, address, latitude, longitude])
     }
+    
+    getStoreByPhone(phone: string) {
+        const queryStr = 'SELECT * FROM STORE_TB WHERE phone = ? AND deleted_at IS NULL'
+        return this.getOne(queryStr, [phone])
+    }
+    
+    checkStore(phone : string, pwd : string) {
+        const queryStr = 'SELECT * FROM STORE_TB WHERE phone = ? AND pwd = ? AND deleted_at IS NULL'
+        return this.getOne(queryStr, [phone, pwd])
+    }
+
 }
 
 export const storeDao = new StoreDao()
--- Covid/src/routers/store.ts
+++ Covid/src/routers/store.ts
@@ -1,18 +1,53 @@
 import { Router } from 'express'
 import { sendErr, sendRes } from '../utils/response-handler'
-import { userDao } from '../daos/StoreDao'
+import { storeDao } from '../daos/StoreDao'
 
 const router = Router()
 
+router.get('/', async (req, res) => {
+    try {
+        const { name, pwd, phone, address, latitude, longitude } = req.query
+        senRes(res, 200, '성공', [{id, name, phone, address, latitude, longitude}])
+    } catch (e) {
+         console.log(e)
+        sendErr(res, e)
+    }
+})
+
 router.post('/register', async (req, res) => {
     try {
-        const { name, phone, address } = req.body
-        // TODO: 이미 존재하는 가게인지 중복 검사.
-        const result = await storeDao.insertUser(name, phone, address)
+        const { name, pwd, phone, address, latitude, longitude } = req.body
+        if (!name || !phone || !address) throw new Error('No Appropriate fields in Request')
+        const found = await storeDao.getStorebyPhone(phone)
+        if (found) {
+            console.log(found)
+            sendRes(res, 201, '이미 존재하는 가게입니다', {
+                updated: true
+            })
+            return
+        }
+        const result = await storeDao.insertStore(name, pwd, phone, address, latitude, longitude)
         if (!result) throw new Error()
         sendRes(res, 200, '', {
             success: true
         })
+    } catch (e) {
+        console.log(e)
+        sendErr(res, e)
+    }
+})
+
+router.post('/login', async(req, res) => {
+    try {
+        const { phone, pwd } = req.body
+        const found = await storeDo.checkStore(phone, pwd)
+        if (!found) {
+            sendRes(res, 201, '회원가입이 되어있지 않은 가게입니다', {updated: true})
+            return
+        }
+        const allowlogin = true
+        if (!allowlogin) throw new Error()
+        sendRes(res, 200, '', { success: true })
     } catch (e) {
         console.log(e)
         sendErr(res, e)
