0cc43cada5fded9bc4626c411185d4ab950b48ec
Ssioo <wooisso@naver.com>
2020-12-18 01:18:14 +0900

    fix typo on store api

--- Covid/src/daos/StoreDao.ts
+++ Covid/src/daos/StoreDao.ts
@@ -1,17 +1,30 @@
 import { BaseDao } from './BaseDao'
 
 class StoreDao extends BaseDao {
-    insertStore(id: string, pwd: string, phone: string, storeName: string, uuid: string, latLng: application/json, address: string) {
-        const queryStr = 'INSERT INTO STORE_TB (id, pwd, phone, storeName, uuid, latLng, address) VALUES (?, ?, ?, ?, ?, ?)'
-        return this.insert(queryStr, [id, pwd, phone, storeName, uuid, latLng, address])
+    insertStore(
+        phone: string,
+        pwd: string,
+        name: string,
+        uuid: string,
+        latitude: number,
+        longitude,
+        address: string
+    ) {
+        const queryStr = 'INSERT INTO STORE_TB (phone, pwd, name, uuid, latitude, longitude, address) VALUES (?, ?, ?, ?, ?, ?, ?)'
+        return this.insert(queryStr, [phone, pwd, name, uuid, latitude, longitude, address])
     }
-    
+
+    getAllStores() {
+        const queryStr = 'SELECT * FROM STORE_TB WHERE deleted_at IS NULL'
+        return this.getAll(queryStr)
+    }
+
     getStoreByPhone(phone: string) {
         const queryStr = 'SELECT * FROM STORE_TB WHERE phone = ? AND deleted_at IS NULL'
         return this.getOne(queryStr, [phone])
     }
-    
-    checkStore(phone : string, pwd : string) {
+
+    getStoreByPhoneAndPwd(phone : string, pwd : string) {
         const queryStr = 'SELECT * FROM STORE_TB WHERE phone = ? AND pwd = ? AND deleted_at IS NULL'
         return this.getOne(queryStr, [phone, pwd])
     }
--- Covid/src/routers/store.ts
+++ Covid/src/routers/store.ts
@@ -6,8 +6,8 @@ const router = Router()
 
 router.get('/', async (req, res) => {
     try {
-        const { id, pwd, phone, storeName, uuid, latLng, address } = req.query
-        senRes(res, 200, '성공', [{id, pwd, phone, storeName, uuid, latLng, address}])
+        const result = await storeDao.getAllStores()
+        sendRes(res, 200, '성공', result)
     } catch (e) {
          console.log(e)
         sendErr(res, e)
@@ -16,19 +16,16 @@ router.get('/', async (req, res) => {
 
 router.post('/register', async (req, res) => {
     try {
-        const { id, pwd, phone, storeName, uuid, latLng, address } = req.body
-        if (!name || !phone || !address) throw new Error('No Appropriate fields in Request')
-        const found = await storeDao.getStorebyPhone(phone)
+        const { pwd, phone, name, uuid, latLng, address } = req.body
+        if (!name || !phone || !address || !pwd || !uuid || !latLng) throw new Error('No Appropriate fields in Request')
+        const found = await storeDao.getStoreByPhone(phone)
         if (found) {
-            console.log(found)
-            sendRes(res, 201, '이미 존재하는 가게입니다', {
-                updated: true
-            })
+            sendRes(res, 400, '이미 존재하는 가게입니다', {})
             return
         }
-        const result = await storeDao.insertStore(id, pwd, phone, storeName, uuid, latLng, address)
+        const result = await storeDao.insertStore(phone, pwd, name, uuid, Number(latLng.latitude), Number(latLng.longitude), address)
         if (!result) throw new Error()
-        sendRes(res, 200, '', {
+        sendRes(res, 200, '환영합니다', {
             success: true
         })
     } catch (e) {
@@ -40,16 +37,14 @@ router.post('/register', async (req, res) => {
 router.post('/login', async(req, res) => {
     try {
         const { phone, pwd } = req.body
-        const found = await storeDo.checkStore(phone, pwd)
+        const found = await storeDao.getStoreByPhoneAndPwd(phone, pwd)
         if (!found) {
-            sendRes(res, 201, '회원가입이 되어있지 않은 가게입니다', {
+            sendRes(res, 400, '계정의 정보가 존재하지 않습니다.', {
                 updated: true
             })
             return
         }
-        const allowlogin = true
-        if (!allowlogin) throw new Error()
-        sendRes(res, 200, '', { success: true })
+        sendRes(res, 200, '', found)
     } catch (e) {
         console.log(e)
         sendErr(res, e)
